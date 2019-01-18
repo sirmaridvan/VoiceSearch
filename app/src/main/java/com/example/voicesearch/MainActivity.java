8c59c6a0d3f2cad42cb6d207ff66c6c4647b00cd
Rıdvan SIRMA <ridvansirma@outlook.com>
2019-01-18 14:16:01 +0300

    Chrome intent call added

--- app/src/main/java/com/example/voicesearch/MainActivity.java
+++ app/src/main/java/com/example/voicesearch/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.voicesearch;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1234;
+    EditText editTextSearchBar;
+    Button buttonSpeak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUI();
+
+        checkRecognizer();
+    }
+
+    private void setUI() {
+        buttonSpeak = findViewById(R.id.speak_button);
+        editTextSearchBar = this.findViewById(R.id.search_bar_edit_text);
+    }
+
+    private void checkRecognizer() {
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (activities.size() == 0) {
+            buttonSpeak.setEnabled(false);
+            buttonSpeak.setText("Recognizer not present");
+        }
+    }
+
+    public void speakButtonClicked(View v) {
+        startVoiceRecognitionActivity();
+    }
+
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice searching...");
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            final ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (!matches.isEmpty()) {
+                String query = matches.get(0);
+                editTextSearchBar.setText(query);
+                String escapedQuery = null;
+                try {
+                    escapedQuery = URLEncoder.encode(query, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    Log.e("MainActivity: ", e.getMessage());
+                }
+                if (escapedQuery != null) {
+                    callBrowserIntent(escapedQuery);
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void callBrowserIntent(String query) {
+        Uri uri = Uri.parse("http://www.google.com/#q=" + query);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
